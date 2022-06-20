49a79c3904ed3d7abf6120dd714e12a32fd5c68c
lottery tether <admin@lotterytether.com>
2022-06-20 18:07:32 +0430

    git fixed v0.5

--- pages/_app.js
+++ pages/_app.js
@@ -14,6 +14,9 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000e0d" />
         <link rel="apple-touch-icon" href="/images/logo192.png" />
+        
+        <meta name="description" content="The first lottery platform on the blockchain" />
+        <meta name="keywords" content="lottery,tether,bet,lottery tether,usdt,usdt lottery,tether lottery" />
 
         <link rel="manifest" href="/manifest.json" />
       </Head>
